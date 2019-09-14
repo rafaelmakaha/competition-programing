144a22b0a2311baae3d5398c83b8ad69cfd9d074
Rafael Makaha <rafael.makaha@gmail.com>
2019-09-14 12:27:21 -0300

    Adds URI 1036 and 1038.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- uri/1036/1036.cpp
+++ uri/1036/1036.cpp
@@ -3,22 +3,20 @@
 using namespace std;
 
 int main(){
-	double a,b,c, delta, r1, r2;
+	float a,b,c, delta, r1, r2;
 
 	cin >> a >> b >> c;
 
-	delta = pow(b,2) -4 * a * c;	
+	delta = b*b-4*a*c;	
 
-	if (delta < 0 || a == 0.0){
+	if (delta < 0 || a == 0){
 		cout << "Impossível calcular\n";
 		return 0;
 	}
 	
 	r1 = (-b + sqrt(delta)) / (2 * a);
 	r2 = (-b - sqrt(delta)) / (2 * a);
-	
-	cout << "R1 = " << fixed << r1 << endl;
-	cout << "R2 = " << fixed << r2 << endl;
+	printf("R1 = %.5f\nR2 = %.5f\n", r1, r2);
 
 	return 0;
 }
