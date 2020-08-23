d32a1948abf3c37eeb5aa1664e3ba6fc1fe182db
Rafael Makaha <rafael.makaha@gmail.com>
2020-08-23 18:02:55 -0300

    Adds 50a.

---
+++ codeforces/problemset/50a.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/problemset/problem/50/A
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int m,n;
+
+    cin >> m >> n;
+    int ans = m * n / 2;
+    cout << ans << endl;
+    return 0;
+}
