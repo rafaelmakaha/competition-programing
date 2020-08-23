6c9038450db4a90b00d00d9ec58b40934823af29
Rafael Makaha <rafael.makaha@gmail.com>
2020-08-23 17:54:22 -0300

    Updates 158a.cpp

--- codeforces/problemset/158a.cpp
+++ codeforces/problemset/158a.cpp
@@ -1,20 +1,27 @@
+// https://codeforces.com/problemset/problem/158/A
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
     int n,k;
-    vector<int> v(50);
+    vector<int> v;
 
     cin >> n >> k;
 
-    int i=0;
-    while(cin >> v[i])
-        i++;
+    int num;
+    while(cin >> num)
+        v.push_back(num);
 
-    for(int i: v){
-        cout << i << " ";
+    int ans=0;
+    for(int i=0; i < int(v.size()); i++){
+        if(v[i] >= v[k-1] && v[i] > 0)
+            ans++;
     }
-    cout << "olouco!";
+
+
+    cout << ans << endl;
+    
     return 0;
 }
