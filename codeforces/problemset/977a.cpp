84c119eac38ed6d9ae43f08ee503b0cc30fde053
Rafael Makaha <rafael.makaha@gmail.com>
2020-08-24 13:25:00 -0300

    Adds 977a

---
+++ codeforces/problemset/977a.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/problemset/problem/977/A
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,k;
+
+    cin >> n >> k;
+
+    while(k>0){
+        if(!(n % 10))
+            n /= 10;
+        else
+            n-=1;
+        k--;
+    }
+    cout << n << endl;
+    return 0;
+}
